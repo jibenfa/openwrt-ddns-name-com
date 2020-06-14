451cbf40a0fac9bad86861ea7598b6c7a8a88bc2
coffeecat <16899547+jibenfa@users.noreply.github.com>
2020-06-14 20:14:19 +0800

    Update ddns_name_com.sh

--- ddns_name_com.sh
+++ ddns_name_com.sh
@@ -7,18 +7,18 @@ HOST="www"
 DOMAIN="example.com"
 URL=${HOST}.${DOMAIN}
 IP=`ping ${URL} -c 1 |awk 'NR==2 {print $4}' |awk -F ':' '{print $1}'`
+#如果安装了dig也可以这样
+#IP=`dig ${DOMAIN} @114.114.114.114 | awk -F "[ ]+" '/IN/{print $1}' | awk 'NR==2 {print $5}'`
 echo "Ip of ${URL} is ${IP}"
- 
-if [ -f /tmp/ddnsResult ]; then
-   OLD_IP=`cat /tmp/ddnsResult | awk '{ print $1}'`
-   if [ "${OLD_IP}" = "${IP}" ]; then
-      exit
-   fi
+LIP=`ifconfig pppoe-wan|awk -F "[: ]+" '/inet addr/{print $4}'`
+echo "Local Ip is ---${LIP}---"
+
+if [ "${LIP}" = "${IP}" ]; then
+   exit
 fi
- 
+
 echo "start ddns refresh"
-IP=`ifconfig pppoe-wan|awk -F "[: ]+" '/inet addr/{print $4}'`
-if [ x"${IP}" != x ]; then
-   curl -u ''""${USER}""':'""${TOKEN}""'' 'https://api.name.com/v4/domains/'""${DOMAIN}""'/records/'""${ID}""'' -X PUT --data '{"host":"'""${HOST}""'","type":"A","answer":"'""${IP}""'","ttl":300}'
-   echo ${IP} > /tmp/ddnsResult
+if [ x"${LIP}" != x ]; then
+   curl -u ''""${USER}""':'""${TOKEN}""'' 'https://api.name.com/v4/domains/'""${DOMAIN}""'/records/'""${ID}""'' -X PUT --data '{"host":"'""${HOST}""'","type":"A","answer":"'""${LIP}""'","ttl":300}'
+   echo ${LIP} > /tmp/ddnsResult
 fi
